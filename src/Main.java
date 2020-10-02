3303ebc97087e92d2cc20e3ce24a218bcaa8569b
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-02 12:55:58 +0300

    Exercise 10

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a number: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.print("Type another number: ");
-        int secondNumber = Integer.parseInt(reader.nextLine());
-        double result = (double)firstNumber/secondNumber;
-        System.out.println("Division: " + firstNumber + " / " + secondNumber + " " + reader);
+        System.out.print("Type the radius: ");
+        int radius = Integer.parseInt(reader.nextLine());
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("Circumference of the circle: " + circumference);
     }
 }
