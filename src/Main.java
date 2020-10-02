af7a7beb548d721cb90b928a2c891456cb5bc0ab
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-02 12:58:15 +0300

    Exercise 14

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type the radius: ");
-        int radius = Integer.parseInt(reader.nextLine());
-        double circumference = 2 * Math.PI * radius;
-        System.out.println("Circumference of the circle: " + circumference);
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        if (number >=0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.printf("The number is not positive.");
+        }
     }
 }
