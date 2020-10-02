01248b89271ea489fb5e8ebdbfd95d9f4169f7d6
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-02 12:47:38 +0300

    Exercise 5

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Jaana Šeffer");
-
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+        int secodnsInAYear = days*hours*minutes*seconds;
+        System.out.println("There are " + secodnsInAYear + " seconds in a year.");
     }
 }
