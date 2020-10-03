4a58a080fe03067ca020faeed20b5ad8471b49f2
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-03 11:57:15 +0300

    Exercise 21

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner reader = new Scanner(System.in);
-        System.out.print("How old are you?");
-        int number = Integer.parseInt(reader.nextLine());
-        if (number >= 0 && number <= 120) {
-            System.out.println("OK");
-        } else if (number < 0 || number > 120) {
-            System.out.println("Impossible!");
+        System.out.print("Type a year: ");
+        int year = Integer.parseInt(reader.nextLine());
+        boolean leapyear = false;
+        if(year % 100 == 0){
+            if(year % 400 == 0){
+                leapyear = true;
+            }
+        } else if (year % 4 == 0){
+            leapyear = true;
+        }
+        if(leapyear){
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
         }
     }
 }
