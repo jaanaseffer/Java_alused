07bb7ccf17bd7baa2861cbba3e0aa644adef01e8
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-03 11:03:54 +0300

    Exercise 16

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type a number: ");
         int number = Integer.parseInt(reader.nextLine());
-        if (number >=0) {
-            System.out.println("The number is positive.");
+        if (number%2 ==0) {
+            System.out.println("The number is even.");
         } else {
-            System.out.println("The number is not positive.");
+            System.out.println("The number is odd.");
         }
     }
 }
