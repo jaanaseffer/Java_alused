eb4b6ae13dea2ba6874e87f2d650aac8da466b05
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-09 14:50:44 +0300

    Exercise 29

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        while (true) {
-            int read = Integer.parseInt(reader.nextLine());
-            if (read == 0) {
-                break;
+        int number = 2;
+        while (number <= 100) {
+            if (number % 2 == 0) {
+                System.out.println(number);
             }
-
-            // DO SOMETHING HERE
-            sum = sum + read; // sum += read;
-
-            System.out.println("Sum now: " + sum);
+            number++;
         }
-        System.out.println("Sum in the end: " + sum);
     }
 }
 
