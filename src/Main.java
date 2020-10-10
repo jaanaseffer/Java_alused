cad8ef4d158383f1bf53d141f5042db69cb2392f
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-10 11:21:53 +0300

    Exercise 15

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
-            Scanner reader = new Scanner(System.in);
-            System.out.print("Type your name: ");
-            String nameOne = reader.nextLine();
-            System.out.print("Type your age: ");
-            int ageOne = Integer.parseInt(input.nextLine());
-            System.out.println();
-            System.out.print("Type your name: ");
-            String nameTwo = reader.nextLine();
-            System.out.print("Type your age: ");
-            int ageTwo = Integer.parseInt(input.nextLine());
-            System.out.println(nameOne + " and " + nameTwo + " are " + (ageOne+ageTwo) + " years old in total.");
+            System.out.print("How old are you?");
+            int age = Integer.parseInt(input.nextLine());
+            if (age >= 18) {
+                System.out.println("You have reached the age of majority!");
+            } else {
+                System.out.println("You have not reached the age of majority yet!");
+            }
     }
 }
