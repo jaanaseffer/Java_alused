16b762d0dcc9cdefa856a2b5f950b7607023ce29
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-10 10:39:05 +0300

    Exercise 7

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            int numberOne = 73457;
-            int numberTwo = 12888;
-            int addition = numberOne + numberTwo;
-            System.out.println(numberOne + " + " + numberTwo + " = " + addition);
+            int numberOne = 277;
+            int numberTwo = 111;
+            int multiplication = numberOne * numberTwo;
+            System.out.println(numberOne + " * " + numberTwo + " = " + multiplication);
     }
 }
