b409d71e3c4caf0efdfd2ed84d68faad3e7434a7
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-10 11:07:02 +0300

    Exercise 11

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            int numberOne = 277;
-            int numberTwo = 111;
-            int multiplication = numberOne * numberTwo;
-            System.out.println(numberOne + " * " + numberTwo + " = " + multiplication);
+            Scanner input = new Scanner(System.in);
+            System.out.print("Type a number: ");
+            int numberOne = Integer.parseInt(input.nextLine());
+            System.out.print("Type another number: ");
+            int numberTwo = Integer.parseInt(input.nextLine());
+            System.out.println("The bigger number of the two numbers given was: " + Math.max(numberOne, numberTwo));
     }
 }
