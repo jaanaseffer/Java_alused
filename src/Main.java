c61c978a98231351cace2bdfd2d5ae7c931fc63a
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-10 10:10:30 +0300

    Exercise 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            System.out.println("Hello world!");
-            System.out.println("(And all the people of the world)");
+            System.out.println("     *     ");
+            System.out.println("    ***    ");
+            System.out.println("   *****   ");
+            System.out.println("  *******  ");
+            System.out.println(" ********* ");
+            System.out.println("     *     ");
     }
 }
