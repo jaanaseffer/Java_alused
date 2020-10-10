a71d27a7f4cc8297d9de3a2204f13bb0a6b5fb30
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-10 11:12:02 +0300

    Exercise 12

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
-            System.out.print("Type a number: ");
-            int numberOne = Integer.parseInt(input.nextLine());
-            System.out.print("Type another number: ");
-            int numberTwo = Integer.parseInt(input.nextLine());
-            System.out.println("Sum of the numbers: " + (numberOne+numberTwo));
+            Scanner reader = new Scanner(System.in);
+            System.out.print("Type your name: ");
+            String nameOne = reader.nextLine();
+            System.out.print("Type your age: ");
+            int ageOne = Integer.parseInt(input.nextLine());
+            System.out.println();
+            System.out.print("Type your name: ");
+            String nameTwo = reader.nextLine();
+            System.out.print("Type your age: ");
+            int ageTwo = Integer.parseInt(input.nextLine());
+            System.out.println(nameOne + " and " + nameTwo + " are " + (ageOne+ageTwo) + " years old in total.");
     }
 }
