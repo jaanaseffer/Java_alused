d1b500a2f86a52a26e0a85a7b1a12cc66c0d29a0
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-13 15:28:35 +0300

    Exercise 38

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Type a number: ");
-            int power = Integer.parseInt(input.nextLine());
-            int number = 2;
-            int i = 0;
-            int sum = 0;
-            while(power >= i) {
-                sum = (int) (sum + Math.pow(number, i));
-                i++;
-            }
-            System.out.println("The result is " + sum);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many?");
+        int number = Integer.parseInt(input.nextLine());
+        int i = 1;
+        while (number >= i) {
+            printText();
+            i++;
+        }
+    }
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 }
