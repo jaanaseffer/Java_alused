814bea94970c08ff1c4a243723286e071a6e9aa8
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-13 10:35:37 +0300

    Exercise 27

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,10 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            Scanner reader = new Scanner(System.in);
-            int sum = 0;
-            int read;
-            System.out.print("Type the first number: ");
-            read = Integer.parseInt(input.nextLine());
-            sum = sum + read;
-            System.out.print("Type the second number: ");
-            read = Integer.parseInt(input.nextLine());
-            sum = sum +read;
-            System.out.print("Type the third number: ");
-            read = Integer.parseInt(input.nextLine());
-            sum = sum +read;
-            System.out.println("Sum: " + sum);
+            int number = 0;
+            while (number < 101) {
+                System.out.println(number);
+                number++;
+            }
     }
 }
