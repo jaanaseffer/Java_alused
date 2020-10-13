574958269310a95c7b526735b8ce805ff3d2dada
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-13 10:46:14 +0300

    Exercise 31

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            int number = 100;
-            while (number > 0) {
-                System.out.println(number);
-                number--;
+            Scanner input = new Scanner(System.in);
+            System.out.println("First: ");
+            int first = Integer.parseInt(input.nextLine());
+            System.out.println("Last: ");
+            int last = Integer.parseInt(input.nextLine());
+            while (first <= last) {
+                System.out.println(first);
+                first++;
             }
     }
 }
