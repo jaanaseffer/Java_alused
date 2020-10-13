29f8c11c913a9d5d8d0457eae7de8405e92a9893
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-13 16:09:34 +0300

    Exercise 40.2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("How many?");
-        int number = Integer.parseInt(input.nextLine());
-        int i = 1;
-        while (number >= i) {
-            printText();
+        printTriangle(4);
+    }
+    public static void printWhitespaces(int size) {
+        int i = 0;
+        while (i < size) {
+            System.out.print(" ");
             i++;
         }
     }
-    public static void printText() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    public static void printStars(int size) {
+        int i = 0;
+        while (i < size){
+            System.out.print("*");
+            i++;
+        }
+    }
+    public static void printTriangle(int size) {
+        int i = 1;
+        while (i <= size) {
+            printWhitespaces(size-i);
+            printStars(i);
+            i++;
+            System.out.println();
+        }
     }
 }
