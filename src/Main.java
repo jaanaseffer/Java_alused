42ab6d525fa3f89cba986d11da19cf27bf6ec75f
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-14 20:58:03 +0300

    Exercise 60.2

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Hallo");
-        list.add("Ciao");
-        list.add("Hello");
-        System.out.println("There are this many items in the list:");
-        System.out.println(countItems(list));
+        ArrayList<String> programmingLanguages = new ArrayList<>();
+        programmingLanguages.add("Pascal");
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("Ruby");
+        programmingLanguages.add("C++");
 
+        print(programmingLanguages);
+
+        removeFirst(programmingLanguages);
+
+        System.out.println();
+
+        print(programmingLanguages);
     }
-    public static int countItems(ArrayList<String> list) {
-        int listSize = list.size();
-        return listSize;
+    public static void print(ArrayList<String> printed) {
+        for (String word: printed) {
+            System.out.println(word);
+        }
+    }
+    public static void removeFirst(ArrayList<String> list) {
+        list.remove(0);
     }
 }
