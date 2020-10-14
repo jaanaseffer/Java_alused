38e3dc72e0c24203bec5cdc9fd9e24d812dbe0ff
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-14 21:33:36 +0300

    Exercise 69

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(2);
-        list.add(7);
-        list.add(2);
-        System.out.println("The greatest number is: " + greatest(list));
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type a text: ");
+        String text = reader.nextLine();
+        if (palindrome(text)) {
+            System.out.println("The text is a palindrome!");
+        } else {
+            System.out.println("The text is not a palindrome!");
+        }
     }
-    public static int greatest(ArrayList<Integer> list) {
-        Collections.sort(list);
-        Collections.reverse(list);
-        return list.get(0);
+    public static boolean palindrome(String text) {
+        String help = "";
+        int i = text.length() - 1;
+        while (i >= 0) {
+            char character = text.charAt(i);
+            help = help + character;
+            i--;
+        }
+        if (text.equals(help)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
