c67d2e63feed1a96d41ada9c4054ccc83a3143ba
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-14 19:01:53 +0300

    Exercise 46

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int answer = least(2, 7);
-        System.out.println("Least: " + answer);
+        double answer = average(4, 3, 6, 1);
+        System.out.println("Average: " + answer);
 
     }
-    public static int least(int number1, int number2) {
-        int least;
-        if (number1 < number2) {
-            least = number1;
-        } else if (number2 < number1) {
-            least = number2;
-        } else {
-            least = number1;
-        }
-        return least;
+    public static double average(int number1, int number2, int number3, int number4) {
+        int sum = number1 + number2 + number3 + number4;
+        double average = (double) sum / 4;
+        return average;
     }
 }
