28ee63f49347f046a43426f0c670c64f98300a71
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-14 19:20:48 +0300

    Exercise 53

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type your name: ");
-        String name = reader.nextLine();
-        if (name.length() < 3) {
-            System.out.println();
-        } else {
-            System.out.println("1. character: " + name.charAt(0));
-            System.out.println("2. character: " + name.charAt(1));
-            System.out.println("3. character: " + name.charAt(2));
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type a word: ");
+        String word = reader.nextLine();
+        System.out.println("Length of the first part: ");
+        int length = Integer.parseInt(input.nextLine());
+        System.out.println("Result: " + word.substring(0, length));
     }
 }
