fa257afa2de56b61198680cdd7e34f95e8ec3a43
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-22 12:48:08 +0300

    Exercise 75.2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(10);
+        DecreasingCounter counter = new DecreasingCounter(2);
 
+        counter.printValue();
+
+        counter.decrease();
         counter.printValue();
 
         counter.decrease();
