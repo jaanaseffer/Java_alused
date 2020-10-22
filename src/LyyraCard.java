9aebc62f2286d842fdddd07f278a4e341a130bbf
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-22 15:33:43 +0300

    Exercise 77.1

---
+++ src/LyyraCard.java
@@ -0,0 +1,13 @@
+public class LyyraCard {
+    private double balance;
+
+    public LyyraCard(double balanceAtStart) {
+        // write code here
+        this.balance = balanceAtStart;
+    }
+
+    public String toString() {
+        // write code here
+         return "This card has " + this.balance + " euros.";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Menu caffee = new Menu();
-
-        caffee.addMeal("Hamburger");
-        caffee.addMeal("Fish'n'Chips");
-        caffee.addMeal("Sauerkraut");
-
-        caffee.printMeals();
-
-        caffee.clearMenu();
-
-        caffee.printMeals();
+        LyyraCard card = new LyyraCard(50);
+        System.out.println(card);
     }
 }
