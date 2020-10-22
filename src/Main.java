40104249103699d7601cf675bcd9b027ef4cba35
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-22 17:21:40 +0300

    Exercise 79.4

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        NumberStatistics stats = new NumberStatistics();
+        NumberStatistics allNumbers = new NumberStatistics();
+        NumberStatistics evenNumbers = new NumberStatistics();
+        NumberStatistics oddNumbers = new NumberStatistics();
         Scanner input = new Scanner(System.in);
 
         System.out.println("Type numbers: ");
         int number = Integer.parseInt((input.nextLine()));
-        while (number > 0) {
-            stats.addNumber(number);
+        while (number != -1) {
+            allNumbers.addNumber(number);
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            } else {
+                oddNumbers.addNumber(number);
+            }
             number = Integer.parseInt((input.nextLine()));
         }
-        System.out.println("sum: " + stats.sum());
+        System.out.println("sum: " + allNumbers.sum());
+        System.out.println("sum of even: " + evenNumbers.sum());
+        System.out.println("sum of odd: " + oddNumbers.sum());
     }
 }
