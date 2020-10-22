d87f3945816cf6277d344c221b93aa47b070848b
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-22 16:47:58 +0300

    Exercise 78.bonus

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner reader = new Scanner(System.in);
         BoundedCounter seconds = new BoundedCounter(59);
         BoundedCounter minutes = new BoundedCounter(59);
         BoundedCounter hours = new BoundedCounter(23);
 
-        System.out.print("seconds: ");
-        int s = Integer.parseInt(reader.nextLine());
-        System.out.print("minutes: ");
-        int m = Integer.parseInt(reader.nextLine());
-        System.out.print("hours: ");
-        int h = Integer.parseInt(reader.nextLine());
+        seconds.setValue(50);
+        minutes.setValue(59);
+        hours.setValue(23);
 
-        seconds.setValue(s);
-        minutes.setValue(m);
-        hours.setValue(h);
-
-        int i = 0;
-        while ( i < 121) {
+        while (true) {
             System.out.println(hours + ":" + minutes + ":" + seconds);
-            // Advance minutes
+            Thread.sleep(1000);
             seconds.next();
             if (seconds.getValue() == 0) {
                 minutes.next();
@@ -29,7 +21,6 @@ public class Main {
                     hours.next();
                 }
             }
-            i++;
         }
     }
 }
