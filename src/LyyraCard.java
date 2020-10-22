fb53e5760c26d38d3c69308def029ffa26d7efc0
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-22 15:43:58 +0300

    Exercise 77.3

--- src/LyyraCard.java
+++ src/LyyraCard.java
@@ -13,11 +13,15 @@ public class LyyraCard {
 
     public void payEconomical() {
         // write code here
-        this.balance = this.balance - 2.5;
+        if (this.balance >= 2.5) {
+            this.balance = this.balance - 2.5;
+        }
     }
 
     public void payGourmet() {
         // write code here
-        this.balance = this.balance - 4.0;
+        if (this.balance >= 4.0) {
+            this.balance = this.balance - 4.0;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        LyyraCard card = new LyyraCard(50);
-        System.out.println(card);
-
-        card.payEconomical();
+        LyyraCard card = new LyyraCard(5);
         System.out.println(card);
 
         card.payGourmet();
-        card.payEconomical();
+        System.out.println(card);
+
+        card.payGourmet();
         System.out.println(card);
     }
 }
