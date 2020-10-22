c0ab7634cc999f1e603da6ef412f17e4cb8a3582
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-22 17:03:02 +0300

    Exercise 79.2

--- src/NumberStatistics.java
+++ src/NumberStatistics.java
@@ -1,15 +1,26 @@
 public class NumberStatistics {
     private int amountOfNumbers;
+    private int sum;
 
     public NumberStatistics() {
         this.amountOfNumbers = 0;
+        this.sum = 0;
     }
 
     public void addNumber(int number) {
         this.amountOfNumbers++;
+        this.sum = this.sum + number;
     }
 
     public int amountOfNumbers() {
         return this.amountOfNumbers;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        return (double)this.sum / this.amountOfNumbers;
     }
 }
