578d1bb2d160a742521197a6186ddf57f7e90e60
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-22 16:25:58 +0300

    Exercise 78.2

--- src/BoundedCounter.java
+++ src/BoundedCounter.java
@@ -19,7 +19,9 @@ public class BoundedCounter {
 
     public String toString() {
         // write code here
-
+        if (this.value < 10) {
+            return "0" + this.value;
+        }
         return "" + this.value;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        BoundedCounter counter = new BoundedCounter(4);
+        BoundedCounter counter = new BoundedCounter(14);
         System.out.println("Value at start: " + counter );
 
         int i = 0;
-        while ( i < 10) {
+        while ( i < 16) {
             counter.next();
             System.out.println("Value: " + counter );
             i++;
