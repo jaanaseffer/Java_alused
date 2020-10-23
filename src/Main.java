59863ba84bfea564e33073a8253d455a2bce5b79
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-23 22:38:48 +0300

    Exercise 98.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {5, 1, 3, 4, 2};
-        printElegantly(array);
+        int[] original = {1, 2, 3, 4};
+        int[] copied = copy(original);
+
+        // change the copied
+        copied[0] = 99;
+
+        // print both
+        System.out.println( "original: " + Arrays.toString(original));
+        System.out.println( "copied: " + Arrays.toString(copied));
     }
 
-    public static void printElegantly(int[] array) {
+    public static int[] copy(int[] array) {
+        int[] newArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            if (i < array.length - 1) {
-                System.out.print(array[i] + ", ");
-            } else {
-                System.out.print(array[i]);
-            }
+            newArray[i] = array[i];
         }
+        return newArray;
     }
 
 }
