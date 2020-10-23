b271e2f544da2836b3e1df0c4ab7ca8a9f229596
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-23 14:53:51 +0300

    Exercise 84.1

---
+++ src/Counter.java
@@ -0,0 +1,46 @@
+public class Counter {
+    private int startingValue;
+    private boolean check;
+    public int value;
+    public Counter(int startingValue, boolean check) {
+        this.startingValue = startingValue;
+        this.check = check;
+        this.value = startingValue;
+    }
+
+    public Counter(int startingValue) {
+        this.startingValue = startingValue;
+        this.check = false;
+        this.value = startingValue;
+    }
+
+    public Counter(boolean check) {
+        this.startingValue = 0;
+        this.check = check;
+        this.value = startingValue;
+    }
+
+    public Counter() {
+        this.startingValue = 0;
+        this.check = false;
+        this.value = startingValue;
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    public void increase() {
+        this.value++;
+    }
+
+    public void decrease() {
+        if (this.check) {
+            if (this.value >= 1) {
+                this.value--;
+            }
+        } else {
+            this.value--;
+        }
+    }
+}
