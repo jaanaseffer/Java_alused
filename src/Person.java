8a1a87c3df6c032906fc0a06631666376aaf33b2
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-23 21:24:00 +0300

    Exercise 93.3

--- src/Person.java
+++ src/Person.java
@@ -35,4 +35,16 @@ public class Person {
     public String toString() {
         return this.name + ", born " + this.birthday;
     }
+
+    public Person(String name, MyDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public Person(String name) {
+        this.name = name;
+        this.birthday = new MyDate(Calendar.getInstance().get(Calendar.DATE),
+                Calendar.getInstance().get(Calendar.MONTH) + 1,
+                Calendar.getInstance().get(Calendar.YEAR));
+    }
 }
