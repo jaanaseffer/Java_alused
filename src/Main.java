6a03770555b7d98acd34df3a3642ea2fc46a9f63
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-10-28 17:53:59 +0200

    KT I ülesanne 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        GuessingGame game = new GuessingGame();
-
-        game.play(1, 10);
-        game.play(10, 99);
+        int size = 10;
+        int rowIndex = 0;
+        int columnIndex = 0;
+        for (rowIndex = 0; rowIndex < size; rowIndex++) {
+            for (columnIndex = 0; columnIndex < size; columnIndex++) {
+                if (rowIndex > columnIndex) {
+                    System.out.print(size-1-rowIndex);
+                } else {
+                    System.out.print(size-1-columnIndex);
+                }
+            }
+            System.out.println();
+        }
     }
 
 }
