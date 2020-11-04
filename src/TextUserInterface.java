ffc8884e520a23d1f0ab543ca1579911841488c7
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-04 17:18:33 +0200

    Week 7 exercise 7.5

--- src/TextUserInterface.java
+++ src/TextUserInterface.java
@@ -10,17 +10,40 @@ public class TextUserInterface {
     }
 
     public void start() {
-        System.out.println("Statement: ");
+        System.out.println("Statements: ");
         System.out.println("quit - quit the interface");
+        System.out.println("translate - asks a word and prints its translation");
+        System.out.println("add - adds a word pair to the dictionary");
 
         while (true) {
+            System.out.println("");
+            System.out.println("Statement: ");
             String input = reader.nextLine();
             if (input.equals("quit")) {
                 System.out.println("Cheers!");
                 break;
-            } else {
+            } else if (input.equals("add")) {
+                System.out.println("In finnish: ");
+                String word = reader.nextLine();
+                System.out.println("Translation: ");
+                String translation = reader.nextLine();
+                add(word, translation);
+            } else if (input.equals("translate")) {
+                System.out.println("Give a word");
+                String word = reader.nextLine();
+                System.out.println("Translation: " + translate(word));
+            }
+            else {
                 System.out.println("Unknown statement");
             }
         }
+    }
+
+    public void add(String word, String translation) {
+        dictionary.add(word, translation);
+    }
+
+    public String translate(String word) {
+        return dictionary.translate(word);
     }
 }
