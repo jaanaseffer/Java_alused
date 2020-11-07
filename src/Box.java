4f3d3498dff47b28fb65471b5656d0527be48fff
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-07 17:06:19 +0200

    Week 8 exercise 11.2

---
+++ src/Box.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Box implements ToBeStored {
+    public ArrayList<ToBeStored> stuff;
+    public double maxWeight;
+
+    public Box(double maxWeight) {
+        this.stuff = new ArrayList<>();
+        this.maxWeight = maxWeight;
+    }
+
+    @Override
+    public double weight() {
+        double weight = 0;
+
+        for (ToBeStored s : stuff) {
+            weight +=s.weight();
+        }
+        return weight;
+    }
+    public void add(ToBeStored object) {
+        if (object.weight() + weight() < maxWeight) {
+            stuff.add(object);
+        }
+    }
+
+    public String toString() {
+        return "Box: " + stuff.size() + "thins, total weight " + weight() + " kg";
+    }
+}
