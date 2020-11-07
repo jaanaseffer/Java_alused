da3eeb743cf98591f3e480fd59654fcf11bfa4e0
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-07 20:28:25 +0200

    Week 8 exercise 15.5

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Hand hand1 = new Hand();
+        ArrayList<Card> cards = new ArrayList<>();
 
-        hand1.add( new Card(2, Card.SPADES) );
-        hand1.add( new Card(14, Card.CLUBS) );
-        hand1.add( new Card(12, Card.HEARTS) );
-        hand1.add( new Card(2, Card.CLUBS) );
+        cards.add( new Card(3, Card.CLUBS) );
+        cards.add( new Card(2, Card.DIAMONDS) );
+        cards.add( new Card(14, Card.CLUBS) );
+        cards.add( new Card(12, Card.HEARTS) );
+        cards.add( new Card(2, Card.CLUBS) );
 
-        Hand hand2 = new Hand();
+        SortAgainstSuit suitSorter = new SortAgainstSuit();
+        cards.sort(suitSorter);
 
-        hand2.add( new Card(11, Card.DIAMONDS) );
-        hand2.add( new Card(11, Card.CLUBS) );
-        hand2.add( new Card(11, Card.HEARTS) );
-
-        int comparison = hand1.compareTo(hand2);
-
-        if ( comparison < 0 ) {
-            System.out.println("the most valuable hand contains the cards");
-            hand2.print();
-        } else if ( comparison > 0 ){
-            System.out.println("the most valuable hand contains the cards");
-            hand1.print();
-        } else {
-            System.out.println("the hands are equally valuable");
+        for (Card c : cards) {
+            System.out.println( c );
         }
     }
 
