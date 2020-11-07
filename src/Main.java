4cbb5a4d8e5489a98927ada0e774f7cb6f5471f6
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-07 18:33:40 +0200

    Week 8 exercise 12.3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Storehouse store = new Storehouse();
-        store.addProduct("coffee", 5, 1);
+        store.addProduct("milk", 3, 10);
+        store.addProduct("coffee", 5, 6);
+        store.addProduct("buttermilk", 2, 20);
+        store.addProduct("jogurt", 2, 20);
 
-        System.out.println("stocks:");
-        System.out.println("coffee:  " + store.stock("coffee"));
-        System.out.println("sugar: " + store.stock("sugar"));
-
-        System.out.println("we take a coffee " + store.take("coffee"));
-        System.out.println("we take a coffee " + store.take("coffee"));
-        System.out.println("we take sugar " + store.take("sugar"));
-
-        System.out.println("stocks:");
-        System.out.println("coffee:  " + store.stock("coffee"));
-        System.out.println("sugar: " + store.stock("sugar"));
+        System.out.println("products:");
+        for (String product : store.products()) {
+            System.out.println(product);
+        }
     }
 
 }
--- src/Storehouse.java
+++ src/Storehouse.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Storehouse {
     public Map<String, Integer> productPrice;
@@ -35,5 +36,9 @@ public class Storehouse {
             return true;
         }
         return false;
+    }
+
+    public Set<String> products() {
+        return productStock.keySet();
     }
 }
