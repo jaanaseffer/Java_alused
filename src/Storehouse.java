f75696e6b668d79622264a58ebd5cc65ff3951c6
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-07 18:30:24 +0200

    Week 8 exercise 12.2

--- src/Storehouse.java
+++ src/Storehouse.java
@@ -3,13 +3,16 @@ import java.util.Map;
 
 public class Storehouse {
     public Map<String, Integer> productPrice;
+    public Map<String, Integer> productStock;
 
     public Storehouse() {
         productPrice = new HashMap<>();
+        productStock = new HashMap<>();
     }
 
     public void addProduct(String product, int price, int stock) {
         productPrice.put(product, price);
+        productStock.put(product, stock);
     }
 
     public int price(String product) {
@@ -17,5 +20,20 @@ public class Storehouse {
             return productPrice.get(product);
         }
         return -99;
+    }
+
+    public int stock(String product) {
+        if (productStock.containsKey(product)) {
+            return productStock.get(product);
+        }
+        return 0;
+    }
+
+    public boolean take(String product) {
+        if (productStock.containsKey(product) && productStock.get(product) > 0) {
+            productStock.put(product, productStock.get(product)-1);
+            return true;
+        }
+        return false;
     }
 }
