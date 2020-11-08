fea330d954ba65dce6c1ef9739854888d0afaaf0
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-08 19:11:16 +0200

    Week 9 exercise 20.1

---
+++ src/application/ConstantSensor.java
@@ -0,0 +1,29 @@
+package application;
+
+public class ConstantSensor implements Sensor {
+    private final int measurement;
+
+    public ConstantSensor(int measurement) {
+        this.measurement = measurement;
+    }
+
+    @Override
+    public boolean isOn() {
+        return true;
+    }
+
+    @Override
+    public void on() {
+
+    }
+
+    @Override
+    public void off() {
+
+    }
+
+    @Override
+    public int measure() {
+        return this.measurement;
+    }
+}
