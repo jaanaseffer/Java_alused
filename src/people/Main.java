7095049af35d1aa00b9635e3d5eef4a90d5d8ae6
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-17 18:30:11 +0200

    Week 10 exercise 29.2

--- src/people/Main.java
+++ src/people/Main.java
@@ -2,9 +2,10 @@ package people;
 
 public class Main {
     public static void main(String[] args) {
-        Person pekka = new Person("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa");
-        Person esko = new Person("Esko Ukkonen", "Mannerheimintie Street 15 00100 Helsinki");
-        System.out.println(pekka);
-        System.out.println(esko);
+        Student olli = new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki");
+        System.out.println(olli);
+        System.out.println("credits " + olli.credits());
+        olli.study();
+        System.out.println("credits "+ olli.credits());
     }
 }
