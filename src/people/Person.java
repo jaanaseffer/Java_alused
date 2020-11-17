16baf56f1df4ec56cdf8b06fb5382a7cfe200eb0
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-17 18:20:15 +0200

    Week 10 exercise 29.1

---
+++ src/people/Person.java
@@ -0,0 +1,16 @@
+package people;
+
+public class Person {
+    private final String name;
+    private final String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n" + "  " + this.address;
+    }
+}
