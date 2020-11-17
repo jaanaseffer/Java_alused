560af12bdc5994cdba51c8f3b6b851c754d9cab9
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-17 17:58:10 +0200

    Week 10 exercise 28.1

---
+++ src/movable/Group.java
@@ -0,0 +1,5 @@
+package movable;
+
+public class Group {
+
+}
---
+++ src/movable/Organism.java
@@ -0,0 +1,21 @@
+package movable;
+
+public class Organism implements Movable {
+    private int y;
+    private int x;
+
+    public Organism(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "x: " + this.x + "; y: " + this.y;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+}
