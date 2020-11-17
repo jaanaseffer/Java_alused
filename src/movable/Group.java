64430bd74389f5902dc2a9cafae307e46ac3a767
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-17 18:07:13 +0200

    Week 10 exercise 28.2

--- src/movable/Group.java
+++ src/movable/Group.java
@@ -1,5 +1,32 @@
 package movable;
 
-public class Group {
+import java.util.ArrayList;
 
+public class Group implements Movable {
+
+    private final ArrayList<Movable> group;
+
+    public Group() {
+        this.group = new ArrayList<>();
+    }
+
+    public void addToGroup(Movable movable) {
+        this.group.add(movable);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder positions = new StringBuilder();
+        for (Movable p : this.group) {
+            positions.append(p.toString()).append("\n");
+        }
+        return positions.toString();
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        for (Movable p: this.group) {
+            p.move(dx, dy);
+        }
+    }
 }
