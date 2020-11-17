002875d1d3a4818f92f04a66ecba66974ce95a67
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-11-17 18:36:35 +0200

    Week 10 exercise 29.5

--- src/people/Main.java
+++ src/people/Main.java
@@ -1,16 +1,20 @@
 package people;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Teacher pekka = new Teacher("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa", 1200);
-        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 Street 00100 Helsinki", 5400);
-        System.out.println( pekka );
-        System.out.println( esko );
+        List<Person> people = new ArrayList<Person>();
+        people.add( new Teacher("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa", 1200) );
+        people.add( new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki") );
 
-        Student olli = new Student("Olli", "Ida Albergintie 1 Street 00400 Helsinki");
-        for ( int i=0; i < 25; i++ ) {
-            olli.study();
+        printDepartment(people);
+    }
+
+    public static void printDepartment(List<Person> people) {
+        for(Person p: people){
+            System.out.println(p.toString());
         }
-        System.out.println( olli );
     }
 }
